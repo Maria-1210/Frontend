e5f579c204016b03d1a77fa3a815686262dad4d2
Maria <mariapazcgh0@gmail.com>
2025-10-08 13:08:43 -0300

    Tab/Health

--- appdeturnos/src/app/tabs/health/health.page.spec.ts
+++ appdeturnos/src/app/tabs/health/health.page.spec.ts
@@ -1,15 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HealthPage } from './health.page';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 describe('HealthPage', () => {
   let component: HealthPage;
   let fixture: ComponentFixture<HealthPage>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), CommonModule],
+      declarations: [HealthPage]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HealthPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- appdeturnos/src/app/tabs/health/health.page.ts
+++ appdeturnos/src/app/tabs/health/health.page.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-health',
+  standalone: true,
+  imports: [IonicModule, CommonModule],
   templateUrl: './health.page.html',
   styleUrls: ['./health.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  
 })
-export class HealthPage implements OnInit {
-
-  constructor() { }
+export class HealthPage {
 
   ngOnInit() {
   }
